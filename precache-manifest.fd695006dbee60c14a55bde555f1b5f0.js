013bbc96f5b5797764b3bbb7a5a5548634817fd7
jhanvimistry <jhanvimistry104@gmail.com>
2024-06-01 17:33:03 +0530

    Updates

---
+++ precache-manifest.fd695006dbee60c14a55bde555f1b5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657b9f9b1cf43a831feb6f44c3847cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33265d36679c7220b8bf",
+    "url": "/static/css/main.f9f690bb.chunk.css"
+  },
+  {
+    "revision": "4f819a6b38aa91a13e43",
+    "url": "/static/js/2.e2144077.chunk.js"
+  },
+  {
+    "revision": "56691e352562c89c8f97",
+    "url": "/static/js/3.3b37501d.chunk.js"
+  },
+  {
+    "revision": "33265d36679c7220b8bf",
+    "url": "/static/js/main.b83ba60c.chunk.js"
+  },
+  {
+    "revision": "69ed8855480a453056c1",
+    "url": "/static/js/runtime~main.becd148e.js"
+  }
+]);
