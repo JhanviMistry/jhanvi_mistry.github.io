3668310b66d1bcbb9d3432957a579a4d9bf406e0
jhanvimistry <jhanvimistry104@gmail.com>
2024-06-01 18:01:42 +0530

    Updates

---
+++ precache-manifest.6c79c2230bc6744a5b3daa42b4c1b599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcb850a1fa2e2427c3cfa7076410639",
+    "url": "/jhanvi_mistry.github.io/index.html"
+  },
+  {
+    "revision": "33265d36679c7220b8bf",
+    "url": "/jhanvi_mistry.github.io/static/css/main.f9f690bb.chunk.css"
+  },
+  {
+    "revision": "12d49e34668e9f97f751",
+    "url": "/jhanvi_mistry.github.io/static/js/2.684cc3b9.chunk.js"
+  },
+  {
+    "revision": "56691e352562c89c8f97",
+    "url": "/jhanvi_mistry.github.io/static/js/3.3b37501d.chunk.js"
+  },
+  {
+    "revision": "33265d36679c7220b8bf",
+    "url": "/jhanvi_mistry.github.io/static/js/main.b83ba60c.chunk.js"
+  },
+  {
+    "revision": "4477332d19ed5d8369a3",
+    "url": "/jhanvi_mistry.github.io/static/js/runtime~main.448c7621.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd695006dbee60c14a55bde555f1b5f0.js"
+  "/jhanvi_mistry.github.io/precache-manifest.6c79c2230bc6744a5b3daa42b4c1b599.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jhanvi_mistry.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
